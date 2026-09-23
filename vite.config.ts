cdb482dce8da3521b5a5067a7fc54342be5c6e6c
诗鸷Temsys <shenshichao920@icloud.com>
2026-09-24 00:36:25 +0800

    cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import press from "fumapress/vite";
 import { fumadocsMdx } from "fumadocs-mdx/vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
-  plugins: [press(), fumadocsMdx(), tailwindcss()],
+  plugins: [press(), fumadocsMdx(), tailwindcss(), cloudflare()],
 });
